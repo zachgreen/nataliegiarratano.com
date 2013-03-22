a7da95d0c11383f84bc5db0b56fb2746e219c04b
Zach Green <zach.green@gmail.com>
2013-03-21 21:31:10 -0400

    changes

---
+++ public/scripts/nataliegiarratano.com.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    //wire up scroll to top functionality
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
